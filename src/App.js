3e5825d6839e1977540648635101671f7cb68d98
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-15 00:20:01 +0100

    Updated App.js with main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/navbar'
+import Home from './pages/home';
+import Shop from './pages/shop'
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <Router>
+        <Navbar />
+        <Switch>
+            <Route path='/' exact component={Home} />
+        </Switch>
+    </Router>
+    );
 }
 
 export default App;
