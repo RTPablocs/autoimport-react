d358de0655c73a874d7f8f2d9b88e7f8fe004f19
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-15 23:28:06 +0100

    Added AdCards Component

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
+import AdCards from "../../components/AdCards";
 
 class Shop extends Component {
     render() {
         return (
-            <div>
-                Hi Shop!
-            </div>
+            <AdCards/>
         );
     }
 }
