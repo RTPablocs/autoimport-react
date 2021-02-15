e793e78426cbd1bdb81bf74fec2e7ba814fb1a35
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-15 11:18:36 +0100

    Added Missing Pages

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import CategoryCard from "../../components/CategoryCard";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <CategoryCard />
+            </div>
+        );
+    }
+}
+
+export default Home;
