33f53da1cab1f4dd07076c5de4562aef7b1b31e2
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-25 23:24:39 +0100

    added component methods (still in progress)

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,16 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AdPage extends Component {
-    constructor(props){
+
+
+    constructor(props) {
         super(props)
+        this.state = {
+            error: null,
+            loaded: false,
+            ad: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/ads/1')
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    loaded: true,
+                    ad: result.data[0]
+                })
+            }, (error) => {
+                this.setState({
+                    error: error,
+                    loaded: false
+                })
+            })
     }
 
     render() {
-        return (
-            <div>
-                {console.log(this.props)}P1
-            </div>
-        );
+        const { error, loaded, ad } = this.state;
+        console.log(ad)
+        if (error) {
+            return (
+                <h2>Error</h2>
+
+            )
+        }
+        if (!loaded) {
+            return (
+                <h2>Loading, please wait </h2>
+            )
+        } else {
+            return (
+                <div>
+                    <h1>{ad.title}</h1>
+                    <p>{ad.desc}</p>
+                </div>
+
+            )
+        }
     }
 }
 
