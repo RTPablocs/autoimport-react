c6f2e35335ced2b8d3db027bf02ed2e5c1e3a0fd
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-25 23:25:13 +0100

    added dynamic route for detail ad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar'
 import Home from './pages/home';
 import Shop from './pages/shop'
 import React from "react";
+import AdPage from './pages/AdPage';
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
         <Navbar />
         <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/ads' component={Shop}/>
+            <Route path='/ads' exact component={Shop}/>
+            <Route path='/ad/:id' component={AdPage}/>
         </Switch>
     </Router>
     );
