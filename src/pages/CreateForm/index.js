524d56899ffceb7a4e66dd3941aa72940301a2d5
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-28 10:37:41 +0100

    Created CreateForm, still in progress

---
+++ src/pages/CreateForm/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class CreateForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            title: '',
+            brand: '',
+            model: '',
+            price: '',
+        }
+    }
+
+    componentDidMount() {
+        const fetchOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+
+        }
+        this.state.id = new RandExp
+    }
+
+    render() {
+        return (
+            <form>
+                <label htmlFor="">Brand</label>
+                <input type="text"/>
+                <label htmlFor="">Model</label>
+                <input type="text"/>
+                <label htmlFor="">Mileage</label>
+                <input type="number"/>
+                <label htmlFor="">Price</label>
+                <input type="number"/>
+                <label htmlFor="">Description</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </form>
+        );
+    }
+}
+
+export default CreateForm;
