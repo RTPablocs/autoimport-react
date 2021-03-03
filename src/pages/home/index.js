f05f96a85d9be4c718b7624fc966b0d137ba10f0
RTPablocs <conejerosorianopablo@gmail.com>
2021-03-03 14:36:19 +0100

    added carousel

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 import CategoryCard from "../../components/CategoryCard";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel'
+import {Photos} from './photos'
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            randomPics: Photos
+        }
+    }
+
     render() {
         return (
             <div>
+                <Carousel>
+                    {this.state.randomPics.map( photo => (
+                        <img src={photo.image} />
+                    ))}
+                </Carousel>
+
                 <CategoryCard />
             </div>
         );
