d80f173557039cbb66b299304b228781e5b5b447
RTPablocs <conejerosorianopablo@gmail.com>
2021-03-03 16:23:13 +0100

    Added carousel and Map

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Renderer from "../../components/Renderer";
+import {Carousel} from "react-responsive-carousel";
+import {Photos} from '../home/photos'
+import MapContainer from "../../components/maps";
+
 
 class AdPage extends Component {
 
@@ -8,18 +13,20 @@ class AdPage extends Component {
         this.state = {
             error: null,
             loaded: false,
-            ad: {}
+            ad: {},
+            photos: Photos
         }
         this.params = props.match.params
     }
 
     componentDidMount() {
+
         fetch(`http://localhost:8080/ads/${this.params.id}`)
             .then(res => res.json())
             .then((result) => {
                 this.setState({
                     loaded: true,
-                    ad: result.data[0]
+                    ad: result.data[0],
                 })
             }, (error) => {
                 this.setState({
@@ -27,10 +34,12 @@ class AdPage extends Component {
                     loaded: false
                 })
             })
+
+
     }
 
     render() {
-        const { error, loaded, ad } = this.state;
+        const {error, loaded, ad} = this.state;
         if (error) {
             return (
                 <h2>Error</h2>
@@ -45,7 +54,15 @@ class AdPage extends Component {
             return (
                 <div>
                     <h1>{ad.title}</h1>
+                    <Carousel>
+                        {this.state.photos.map(photo => (
+                            <img src={photo.image}/>
+                        ))}
+
+                    </Carousel>
+
                     <p>{ad.desc}</p>
+                    <MapContainer/>
                 </div>
 
             )
